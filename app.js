1ff49d055f4aabf0028aa7d39ef2053021f480ab
vietlinhg4 <vietlinhg7@gmail.com>
2023-12-29 21:53:51 +0700

    create login page

--- app.js
+++ app.js
@@ -4,43 +4,47 @@ const User = require('./models/user');
 
 const app = express();
 const PORT = 4000;
+const expressHbs = require("express-handlebars");
+
+var authRouter = require('./routes/auth');
+
+// Thiet lap thu muc Static
+app.use(express.static(__dirname + "/html"));
+
+//Cau hinh su dung View Template
+app.engine(
+  "hbs", 
+  expressHbs.engine()
+);
+app.set("view engine", "hbs");
 
 const uri = "mongodb+srv://nhom10:web21ktpm@cluster0.uveminn.mongodb.net/nhom10?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const addUser = async () => {
-  const newUser = new User({
-    userID: 'd01',
-    password: 'password',
-    chucvu: 'dan',
-    hoTen: 'Le Dinh Dan',
-    nsinh: new Date('1990-11-21'),
-    email: 'tonytran12@gmail.com',
-    sdt: '0156156165',
-    phuong: null,
-    quan: null,
-  });
+// const addUser = async () => {
+//   const newUser = new User({
+//     userID: 'd01',
+//     password: 'password',
+//     chucvu: 'dan',
+//     hoTen: 'Le Dinh Dan',
+//     nsinh: new Date('1990-11-21'),
+//     email: 'tonytran12@gmail.com',
+//     sdt: '0156156165',
+//     phuong: null,
+//     quan: null,
+//   });
 
-  try {
-    const savedUser = await newUser.save();
-    console.log(`User ${savedUser.userID} has been added.`);
-  } catch (error) {
-    console.error(`Error occurred while adding user: ${error}`);
-  }
-};
+//   try {
+//     const savedUser = await newUser.save();
+//     console.log(`User ${savedUser.userID} has been added.`);
+//   } catch (error) {
+//     console.error(`Error occurred while adding user: ${error}`);
+//   }
+// };
 
-addUser();
+// addUser();
 
-
-app.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'public, max-age=604800, stale-while-revalidate=604800');
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  next();
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use('/', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('login', {
+    layout: false,
+  })
+});
+
+module.exports = router;
