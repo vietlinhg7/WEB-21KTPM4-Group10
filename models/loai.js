9750c6e0436207ccc2a6d5616087e0700bde1226
vietlinhg4 <vietlinhg7@gmail.com>
2024-01-02 17:21:26 +0700

    editing sidebar

--- models/loai.js
+++ models/loai.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const loaiSchema = new Schema({
-    loaiID: { type: String, required: true },
-    loai: String,
+    loai: { type: String, required: true },
 });
 
 const Loai = mongoose.model('Loai', loaiSchema);
