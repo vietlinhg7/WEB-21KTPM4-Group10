e01d5ceac9480c6db767a72fc47e11951cf1f26d
quocti2003 <lejack526@gmail.com>
2024-01-11 11:40:16 +0700

    request merge

--- controllers/authController.js
+++ controllers/authController.js
@@ -230,17 +230,17 @@ controller.DDQCmap = async (req, res) => {
     });
 }
 controller.showLocation = async (req, res) => {
-    // res.render('Phuong-ChinhSuaDDQC', {
-    //     layout: 'So'
-    // });
+    res.render('Phuong-YCCP-Censored-list', {
+        layout: 'So'
+    });
     // res.render('DanhSachBaoCao', {
     //     layout: 'So'
     // });
-    let location = await Location.find({});
-    res.render('So-DDQC', {
-        layout: 'So',
-        location: location
-    });
+    // let location = await Location.find({});
+    // res.render('So-DDQC', {
+    //     layout: 'So',
+    //     location: location
+    // });
     
 }
 controller.themHinhThucQC = async (req, res) => {
