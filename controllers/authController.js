ba2e3fffc3ce4dacddf26293e24791e95dcbb369
quocti2003 <lejack526@gmail.com>
2024-01-11 11:55:05 +0700

    update UI Phuong-YCCP-Uncensored-list and Censored-list

--- controllers/authController.js
+++ controllers/authController.js
@@ -238,18 +238,23 @@ controller.DDQCmap = async (req, res) => {
     });
 }
 controller.showLocation = async (req, res) => {
-    // res.render('Phuong-ChinhSuaDDQC', {
+    // res.render('Phuong-YCCP-Censored-list', {
     //     layout: 'So'
     // });
     // res.render('DanhSachBaoCao', {
     //     layout: 'So'
     // });
+    // let location = await Location.find({});
+    // res.render('So-DDQC', {
+    //     layout: 'So',
+    //     location: location
+    // });
+
     let location = await Location.find({});
     res.render('So-DDQC', {
         layout: 'So',
         location: location
     });
-    
 }
 controller.themHinhThucQC = async (req, res) => {
 
